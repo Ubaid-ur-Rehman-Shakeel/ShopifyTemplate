b1b69d25bf483fc4278cd0dd1299a9f9e082540c
Ubaid-ur-Rehman <ubaidurrehman14827@gmail.com>
2023-09-21 16:43:03 +0500

    Initial Setup Completed

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default async function HomePage() {
       <ThreeItemGrid />
       <Suspense>
         <Carousel />
-     <h1>Helloo World</h1>
+     
         <Suspense>
           <Footer />
         </Suspense>

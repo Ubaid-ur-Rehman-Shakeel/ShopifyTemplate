43091b78cb9f6214e1973ceab3702586c6cee2f7
abdullah <abdullaharain1309@gmail.com>
2023-09-26 21:53:09 +0500

    foter added

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,70 +1,57 @@
-import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import { ChevronRight, Facebook, SendHorizontal } from 'lucide-react';
+import { BsFacebook } from 'react-icons/bs';
+import { AiFillTwitterCircle } from 'react-icons/ai';
+import { AiFillInstagram } from 'react-icons/ai';
+import Image from "next/image";
 
-import FooterMenu from 'components/layout/footer-menu';
-import LogoSquare from 'components/logo-square';
-import { getMenu } from 'lib/shopify';
-import { Suspense } from 'react';
-
-const { COMPANY_NAME, SITE_NAME } = process.env;
-
-export default async function Footer() {
-  const currentYear = new Date().getFullYear();
-  const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
-  const skeleton = 'w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700';
-  const menu = await getMenu('next-js-frontend-footer-menu');
-  const copyrightName = COMPANY_NAME || SITE_NAME || '';
-
+export default function Foter() {
   return (
-    <footer className="text-sm text-neutral-500 dark:text-neutral-400">
-      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0">
-        <div>
-          <Link className="flex items-center gap-2 text-black dark:text-white md:pt-1" href="/">
-            <LogoSquare size="sm" />
-            <span className="uppercase">{SITE_NAME}</span>
-          </Link>
-        </div>
-        <Suspense
-          fallback={
-            <div className="flex h-[188px] w-[200px] flex-col gap-2">
-              <div className={skeleton} />
-              <div className={skeleton} />
-              <div className={skeleton} />
-              <div className={skeleton} />
-              <div className={skeleton} />
-              <div className={skeleton} />
-            </div>
-          }
-        >
-          <FooterMenu menu={menu} />
-        </Suspense>
-        <div className="md:ml-auto">
-          <a
-            className="flex h-8 w-max flex-none items-center justify-center rounded-md border border-neutral-200 bg-white text-xs text-black dark:border-neutral-700 dark:bg-black dark:text-white"
-            aria-label="Deploy on Vercel"
-            href="https://vercel.com/templates/next.js/nextjs-commerce"
-          >
-            <span className="px-3">▲</span>
-            <hr className="h-full border-r border-neutral-200 dark:border-neutral-700" />
-            <span className="px-3">Deploy</span>
-          </a>
-        </div>
+    <main className='bg-main pt-5 mt-5'>
+    <section className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:flex justify-evenly mt-10 p-5 gap-10'>
+
+      <div className="text-subsec">
+        <h2 className='text-2xl font-bold'>Shoetopia</h2>
+        <p className='mt-5 lg:w-56 w-auto mr-3'>Find your perfect pair of shoes for any occasion.</p>
       </div>
-      <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
-        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
-          <p>
-            &copy; {copyrightDate} {copyrightName}
-            {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved.
-          </p>
-          <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
-          <p>Designed in California</p>
-          <p className="md:ml-auto">
-            Crafted by{' '}
-            <a href="https://vercel.com" className="text-black dark:text-white">
-              ▲ Vercel
-            </a>
-          </p>
-        </div>
+
+      <div className="text-subsec">
+        <h3 className='font-semibold text-xl mb-3'>Product</h3>
+        <p>Medical shoes</p>
+        <p>Sneakers</p>
+        <p>Boots</p>
       </div>
-    </footer>
-  );
+
+      <div className="text-subsec">
+        <h3 className='font-semibold text-xl mb-3'>Support</h3>
+        <p>Support Carrier</p>
+        <p>24th Service</p>
+        <p>Quick Chat</p>
+      </div>
+      
+      <div className="text-subsec">
+        <h3 className='font-semibold text-xl mb-3'>Company</h3>
+        <p>Abouts</p>
+        <p>Terms and Conditions</p>
+        <p>Privacy Policy</p>
+      </div>
+
+
+    </section>
+
+        <div className='flex gap-1 items-center lg:mx-32 mx-6 mt-5 text-subsec'>
+           <ul className="flex gap-1">
+            <li><BsFacebook size={18}/></li>
+            <li><AiFillTwitterCircle size={20}/></li>
+            <li><AiFillInstagram size={20}/></li>
+            </ul>
+            <p className="">/Shoetopia</p>
+        </div>
+
+    <section className='mt-4 text-center py-2 bg-sub text-main'>  
+        <p>© 2023 Panaverse. All rights reserved</p>     
+    </section>
+
+    </main>
+  )
 }

e77ee04e8d535276122e394d0bac848748355558
abdullah <abdullaharain1309@gmail.com>
2023-09-27 22:03:38 +0500

    contact-us page completed

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -1,7 +1,7 @@
 "use client"
+import Slider from 'react-slick';
+import Slide from './slide';
 import { useTypewriter } from 'react-simple-typewriter';
-import Slider from "react-slick";
-import Slide from "./slide";
 
 const Hero = () => {
     const settings = {
@@ -10,31 +10,12 @@ const Hero = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed:5000,
+        autoplaySpeed: 5000,
         pauseOnHover: false,
-        arrows:false
-    };
+        arrows: false,
+        adaptiveHeight: true,
 
-    const slideData = [
-        {
-            id: 0,
-            image: '/blue-shoe-banner.jpg',
-            showButton: true,
-            showHeading: true,
-        },
-        {
-            id: 1,
-            image: '/blue-sale-banner2.jpg',
-            showButton: false,
-            showHeading: false,
-        },
-        {
-            id: 2,
-            image: '/banner-3.jpg',
-            showButton: false,
-            showHeading:false
-        }
-    ];
+    };
 
     const [text1] = useTypewriter({
         words: [
@@ -44,28 +25,75 @@ const Hero = () => {
         delaySpeed: 1000,
     });
 
+    const slideData = [
+        {
+            id: 0,
+            image: '/banner-5.jpg',
+            showButton: true,
+            showHeading: true,
+            text: text1,
+            buttonStyle: "relative left-[22vw] mt-2 lg:mt-10 lg:left-[22vw] bg-text outline-main text-sub hover:bg-sub hover:text-text",
+            buttonText: "Buy now!",
+            heading: "Urban Treasures",
+            sectionStyle: "absolute top-[10vh] md:top-[25vh] md:left-[5vw]",
+            headingStyle: "text-3xl lg:text-8xl md:text-6xl  font-semibold px-10 z-20 lg:ml-10 text-[#FFFFFF]",
+            textStyle: "text-1xl lg:text-5xl md:text-4xl font-semibold px-10 z-20 mt-2 lg:mt-5 lg:ml-10 text-[#FFFFFF]"
+
+        },
+        {
+            id: 1,
+            image: '/banner-7.jpg',
+            showButton: true,
+            showHeading: true,
+            text: "lorem ipsum lorem ipsum",
+            buttonStyle: "relative left-[22vw] mt-5 lg:left-[20vw] bg-text outline-main text-sub hover:bg-sub hover:text-text",
+            buttonText: "Explore",
+            heading: "New Arrival",
+            sectionStyle: "absolute top-[12vh] md:top-[25vh] md:left-[15vw]",
+            headingStyle: "text-3xl lg:text-8xl md:text-6xl  font-semibold px-10 z-20 lg:ml-10 text-[#FFFFFF]",
+            textStyle: "text-1xl lg:text-5xl md:text-4xl font-semibold px-10 z-20 mt-2  lg:ml-10 text-[#FFFFFF]"
+        },
+        {
+            id: 2,
+            image: '/banner-8.jpg',
+            showButton: true,
+            showHeading: true,
+            text: "Check out",
+            buttonStyle: "relative left-[15vw] mt-2  lg:left-[15vw] lg:mt-10 bg-text outline-main text-sub hover:bg-sub hover:text-text",
+            buttonText: "Sale%",
+            heading: "Sale 50%",
+            sectionStyle: "absolute top-[8vh] left-[45vw] md:top-[25vh] md:left-[50vw]",
+            headingStyle: "text-3xl lg:text-8xl md:text-6xl  font-semibold px-10 z-20 lg:ml-10 text-[#FFFFFF]",
+            textStyle: "text-1xl lg:text-5xl md:text-4xl font-semibold px-10 z-20 mt-2 lg:ml-10 text-[#FFFFFF]"
+        }
+    ];
+
+
+
     return (
 
-            <div className='lg:pt-0  lg:top'>
-                <Slider {...settings}>
-                    {slideData.map((item) => (
-                        <Slide
-                            key={item.id}
-                            image={item.image}
-                            showButton={item.showButton}
-                            buttonText='Buy now!'
-                            buttonStyle='relative left-[22vw] mt-10 lg:left-[25vw] bg-sub outline-main text-text hover:bg-white hover:text-sub'
-                            showText={item.showHeading}
-                            text={text1}
-                            heading='Urban Treasures'
-                            sectionStyle='absolute top-[25vh] left-[15vw]'
+        <div className='lg:pt-0  lg:top'>
+            <Slider {...settings}>
+                {slideData.map((item) => (
+                    <Slide
+                        key={item.id}
+                        image={item.image}
+                        showButton={item.showButton}
+                        buttonText={item.buttonText}
+                        buttonStyle={item.buttonStyle}
+                        showText={item.showHeading}
+                        text={item.text}
+                        heading={item.heading}
+                        sectionStyle={item.sectionStyle}
+                        headingStyle={item.headingStyle}
+                        textStyle={item.textStyle}
 
 
-                        />
-                    ))}
-                </Slider>
-                </div>
-    
+                    />
+                ))}
+            </Slider>
+        </div>
+
 
     );
 };
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section from './Section'
+import Contacts from './Contacts'
+import { FaqThree } from './Faq'
+
+export default function page() {
+  return (
+    <div>
+      <Section/>
+      <Contacts/>
+      <FaqThree/>
+    </div>
+  )
+}
